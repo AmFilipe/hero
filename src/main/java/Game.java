4cf53b85e406b38d7bffcc543236eaca1b2fa710
AmFilipe <A044351@umaia.pt>
2024-09-24 22:09:47 +0100

    ex7

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,7 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -7,6 +9,9 @@ import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
 
 public class Game {
+
+    private int x = 10;
+    private int y = 10;
     private final TerminalScreen screen;
     public Game(int  width, int height) throws IOException {
         // Terminal terminal = new DefaultTerminalFactory().createTerminal();
@@ -18,16 +23,49 @@ public class Game {
         TerminalSize terminalSize = new TerminalSize(width, height);
         // DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
     }
+
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(0, 0, TextCharacter.fromCharacter('X')[0]);
-        screen.setCharacter(0, 30, TextCharacter.fromCharacter('X')[0]);
-        screen.setCharacter(30, 0, TextCharacter.fromCharacter('X')[0]);
-        screen.setCharacter(30, 30, TextCharacter.fromCharacter('X')[0]);
+        //screen.setCharacter(0, 0, TextCharacter.fromCharacter('X')[0]);
+        //screen.setCharacter(0, 30, TextCharacter.fromCharacter('X')[0]);
+        //screen.setCharacter(30, 0, TextCharacter.fromCharacter('X')[0]);
+        //screen.setCharacter(30, 30, TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
+
     }
+
+
     public void run() throws IOException {
-        draw();
+        while (true) {
+            draw();
+            KeyStroke key = screen.readInput();
+            if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q')
+                screen.close();
+            if (key.getKeyType() == KeyType.EOF)
+                break;
+            processKey(key);
+        }
+    }
+
+
+    private void processKey(KeyStroke key) {
+        System.out.println(key);
+        String keyT = key.getKeyType().toString();
+        switch (keyT) {
+            case "ArrowUp":
+                y = y - 1;
+                break;
+            case "ArrowDown":
+                y = y + 1;
+                break;
+            case "ArrowLeft":
+                x = x - 1;
+                break;
+            case "ArrowRight":
+                x = x + 1;
+                break;
+        }
     }
 }
 
